935f4f75760d7dffb5864c23cdbc01aeb1fc190d
Terry Babyuk <tbabyuk@gmail.com>
2022-11-09 23:25:01 -0500

    added levels info as an import, added more levels to levels.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+import { LEVELS } from "./levels.js";
+
 // DOM ELEMENTS
 
 const levelSelector = document.querySelector(".level-selector");
@@ -7,16 +9,6 @@ const nextChallengeBtn = document.querySelector("#next-challenge-btn");
 const selectedLevel = document.querySelector("#selected-level");
 const message = document.querySelector("#message");
 const totalChallenges = document.querySelector("#total-challenges");
-
-
-// LEVELS
-
-const LEVELS = {
-    prepA: ["C pentascale", "G pentascale", "D pentascale", "A min pentascale", "C major triad"],
-    prepB: ["D pentascale", "A pentascale", "F pentascale", "E min pentascale", "D min pentascale", "A min (nat.) pentascale", "C maj contrary motion", "C maj triads", "G maj triads", "A min triads"],
-    lvl1: ["C maj scale", "G maj scale", "F maj scale", "A min natural scale", "A min harmonic scale", "E min natural scale", "E min harmonic scale", "D min natural scale", "D min harmonic scale", "C maj contrary motion scale", "Chromatic scale starting on C", "C maj triads (broken)", "C maj triads (solid)", "G maj triads (broken)", "G maj triads (solid)", "F maj triads (broken)", "F maj triads (solid)", "A min triads (broken)", "A min triads (solid)", "E min triads (broken)", "E min triads (solid)", "D min triads (broken)", "D min triads (solid)"],
-    lvl2: ["G maj scale", "F maj scale", "B♭ maj scale", "E min harmonic scale", "E min melodic scale", "D min harmonic scale", "D min melodic scale", "G min harmonic scale", "G min melodic scale", "C maj formula pattern", "G maj formula pattern", "Chromatic scale starting on G", "G maj triads (broken)", "G maj triads (solid)", "F maj triads (broken)", "F maj triads (solid)", "B♭ maj triads (broken)", "B♭ maj triads (solid)", "E min triads (broken)", "E min triads (solid)", "D min triads (broken)", "D min triads (solid)", "G min triads (broken)", "G min triads (solid)"]
-}
 
 
 // EVENT LISTENERS
--- technical.js
+++ technical.js
@@ -6,18 +6,7 @@ const LEVELS = {
     lvl1: ["C maj scale", "G maj scale", "F maj scale", "A min natural scale", "A min harmonic scale", "E min natural scale", "E min harmonic scale", "D min natural scale", "D min harmonic scale", "C maj contrary motion scale", "Chromatic scale starting on C", "C maj triads (broken)", "C maj triads (solid)", "G maj triads (broken)", "G maj triads (solid)", "F maj triads (broken)", "F maj triads (solid)", "A min triads (broken)", "A min triads (solid)", "E min triads (broken)", "E min triads (solid)", "D min triads (broken)", "D min triads (solid)"]
 }
 
-    // lvl2: {
-    //     technical: "<b>SCALES:</b><br> major: G, F, B♭<br>minor: E, D, G (har. & mel.)<br><br><b>FORMULA PATTERN:</b><br>C, G major<br><br><b>CHROMATIC:</b><br>starting on G<br><br><b>TRIADS:</b><br>major: G, F, B♭<br>minor: E, D, G",
-    // },
-    // lvl3: {
-    //     technical: "<b>SCALES:</b><br> major: D, F, B♭<br>minor: B, D, G (har. & mel.)<br><br><b>FORMULA PATTERN:</b><br>D major<br><br><b>CHROMATIC:</b><br>starting on D<br><br><b>TRIADS:</b><br>major: D, F, B♭<br>minor: B, D, G",
-    // },
-    // lvl4: {
-    //     technical: "<b>SCALES:</b><br> major: D, A, B♭, E♭<br>minor: B, G, C (har. & mel.)<br><br><b>FORMULA PATTERN:</b><br>C minor (har.)<br><br><b>CHROMATIC:</b><br>starting on C<br><br><b>TRIADS:</b><br>major: D, A, B♭, E♭<br>minor: B, G, C",
-    // },
-    // lvl5: {
-    //     technical: "<b>SCALES:</b><br> major: A, E, F, A♭<br>minor: A, E, F (har. & mel.)<br><br><b>FORMULA PATTERN:</b><br>A major,<br> A minor (har.)<br><br><b>CHROMATIC:</b><br>starting on A, F<br><br><b>TRIADS:</b><br>major: A, E, F, A♭<br>minor: A, E, F<br><br><b>DOMINANT 7th CHORDS:</b><br>major: A, E, F, A♭<br><br><b>ARPEGGIOS:</b><br>major: A, E, F, A♭<br>minor: A, E F",
-    // },
+
     // lvl6: {
     //     technical: "<b>SCALES:</b><br> major: G, E, B, D♭<br>minor: G, E, B, C♯ (har. & mel.)<br><br><b>FORMULA PATTERN:</b><br>E major,<br> E minor (har.)<br><br><b>CHROMATIC:</b><br>starting on E, D♭<br><br><b>TRIADS:</b><br>major: G, E, B, D♭<br>minor: G, E, B, C♯<br><br><b>DOMINANT 7th CHORDS:</b><br>major: G, E, B, D♭<br><br><b>LEADING-TONE DIMINISHED 7th CHORDS:</b><br>G, E, B, C♯ minor<br><br><b>ARPEGGIOS (tonic):</b><br>major: G, E, B, D♭<br>minor: G, E, B, C♯<br><br><b>ARPEGGIOS (dom 7th):</b><br>G, E, B, D♭<br><br><b>ARPEGGIOS (dim 7th):</b><br>G, E, B, C♯ minor",
     // },
