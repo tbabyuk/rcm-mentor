bdc2cc5f9697637246e518d7e5a9e34636a1dc7b
Terry Babyuk <tbabyuk@gmail.com>
2022-11-12 13:17:50 -0500

    adjusted UI, implemented responsiveness, version 1 completed

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const levelSelector = document.querySelector("#level-selector");
 const randomChallenge = document.querySelector("#random-challenge");
 const nextChallengeBtn = document.querySelector("#next-challenge-btn");
 const selectedLevel = document.querySelector("#selected-level");
-const message = document.querySelector("#message");
 const totalChallenges = document.querySelector("#total-challenges");
 const currentYear = document.querySelector("#year");
 
@@ -20,23 +19,17 @@ levelSelector.addEventListener("change", () => {
     nextChallengeBtn.className = "btn btn-primary active";
     nextChallengeBtn.focus();
     selectedLevel.innerText = levelSelector.value;
-    message.className = "text-success";
-    message.innerText = "press button or 'space' key for new challenge";
+    randomChallenge.innerText = "press button"
     showTotalChallenges(levelSelector.value);
 })
 
-nextChallengeBtn.addEventListener("click", generateChallenge)
-
-nextChallengeBtn.addEventListener("blur", () => {
-    nextChallengeBtn.classList.add("disabled");
-    levelSelector.value = "initial";
-    levelSelector.focus();
-    selectedLevel.innerText = "";
-    randomChallenge.innerText = "";
-    message.className = "text-danger"
-    message.innerText = "select a level to start practicing"
+document.addEventListener('keyup', (e) => {
+    if(e.key == " ") {
+        nextChallengeBtn.focus();
+    }
 })
 
+nextChallengeBtn.addEventListener("click", generateChallenge)
 
 
 // FUNCTIONS
